fa1b2dd015469eedeb84474dd3120bbb62dc4b1a
ELjoOo <youcifer.777@gmail.com>
2023-12-05 03:17:38 +0200

    0x03. Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+        return my_tuple
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
