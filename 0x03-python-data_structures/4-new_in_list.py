dac7ef78e3bbf60309e6e031d49a68fda2ebf5b4
ELjoOo <youcifer.777@gmail.com>
2023-12-05 02:52:46 +0200

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,4 +6,4 @@ def new_in_list(my_list, idx, element):
         return my_list
     new_list = my_list[:]
     new_list[idx] = element
-    print("{}\n{}".format(new_list, my_list))
+    return new_list
