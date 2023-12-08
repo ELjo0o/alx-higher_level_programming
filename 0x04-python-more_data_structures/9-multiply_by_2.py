58f91e82fb9ed15562b7f9fccfe0a75b7f6a8b6b
ELjoOo <youcifer.777@gmail.com>
2023-12-09 01:55:16 +0200

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for i in a_dicionary:
-        i = i * 2
-
+    new_d = {}
+    for i in a_dictionary:
+        new_d[i] = a_dictionary[i] * 2
+    return new_d
